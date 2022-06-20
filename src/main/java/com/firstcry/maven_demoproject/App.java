d35b81e0cf57494e90a7a73584907d3c4dea6750
thirg01 <thirg01@gmail.com>
2022-06-20 22:29:08 +0530

    first commit

---
+++ src/main/java/com/firstcry/maven_demoproject/App.java
@@ -0,0 +1,13 @@
+package com.firstcry.maven_demoproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
